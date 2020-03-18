29a2e511e997331be096cd4a2f71cb2c3f7bf1de
Gordon Inggs <gordon.e.inggs@ieee.org>
2020-03-18 15:00:23 +0200

    Adding README and utility scripts.
    What is needed to get going.

--- bin/build-dash.sh
+++ bin/build-dash.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-# ToDo script for copying data into dist directory and zipping it up?
+set -e
+
+BUILD_DIR_PATH="$PWD"/build
+mkdir -p $BUILD_DIR_PATH
+
+DESTINATION_PATH=${2:-$BUILD_DIR_PATH}
+
+# Copying across assets
+ASSETS_PATH="./assets"
+cp -r $ASSETS_PATH $BUILD_DIR_PATH/$ASSETS_PATH/
+
+# Copying html
+cp *.html $BUILD_DIR_PATH/
+
+# Zipping up
+DIST_DEFAULT_PATH="./dist"
+DIST_PATH=${3:-$DIST_DEFAULT_PATH}
+mkdir -p $DIST_PATH
+
+file=../"$DIST_PATH"/covid-19-dashboard.zip
+
+cd $DESTINATION_PATH
+# Removing old version
+if test -f "$file"; then
+    rm $file
+fi
+
+# Creating new version
+zip -r $file $ASSETS_PATH *.html
+cd -
