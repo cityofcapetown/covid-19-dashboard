8a37fde62451fd5bef2415546151e836c3859328
Gordon Inggs <gordon.e.inggs@ieee.org>
2020-03-20 11:38:50 +0200

    Updating City stat widgets dynamically from JSON.

---
+++ assets/js/update_dash.js
@@ -0,0 +1,13 @@
+function updateCityDashboard() {
+    $.getJSON('widgets/latest_values.json', function( data ) {
+        var rsa_latest_update = data.rsa_latest_update;
+        var rsa_latest_tested = data.rsa_latest_tested;
+        var rsa_latest_confirmed = data.rsa_latest_confirmed;
+        var rsa_latest_deaths = data.rsa_latest_deaths;
+
+        $("#last_updated").text(rsa_latest_update);
+        $("#tests_conducted").text(rsa_latest_tested);
+        $("#confirmed_cases").text(rsa_latest_confirmed);
+        $("#deaths").text(rsa_latest_deaths);
+    });
+};
