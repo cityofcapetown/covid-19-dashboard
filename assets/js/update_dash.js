9c621bad73594b7d682731883f469aee148571b8
Gordon Inggs <gordon.e.inggs@ieee.org>
2020-03-20 11:47:56 +0200

    Updating SA stat widgets dynamically from JSON.

--- assets/js/update_dash.js
+++ assets/js/update_dash.js
@@ -11,3 +11,17 @@ function updateCityDashboard() {
         $("#deaths").text(rsa_latest_deaths);
     });
 };
+
+function updateSADashboard() {
+    $.getJSON('widgets/latest_values.json', function( data ) {
+        var rsa_latest_update = data.rsa_latest_update;
+        var rsa_latest_tested = data.rsa_latest_tested;
+        var rsa_latest_confirmed = data.rsa_latest_confirmed;
+        var rsa_latest_deaths = data.rsa_latest_deaths;
+
+        $("#last_updated").text(rsa_latest_update);
+        $("#tests_conducted").text(rsa_latest_tested);
+        $("#confirmed_cases").text(rsa_latest_confirmed);
+        $("#deaths").text(rsa_latest_deaths);
+    });
+};
