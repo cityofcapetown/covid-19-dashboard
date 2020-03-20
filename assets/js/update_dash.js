c5efa154b95e54556e2e96282d8ee6be66c4239b
Gordon Inggs <gordon.e.inggs@ieee.org>
2020-03-20 11:52:07 +0200

    Simplifying variable names

--- assets/js/update_dash.js
+++ assets/js/update_dash.js
@@ -1,27 +1,27 @@
 function updateCityDashboard() {
     $.getJSON('widgets/latest_values.json', function( data ) {
-        var rsa_latest_update = data.rsa_latest_update;
-        var rsa_latest_tested = data.rsa_latest_tested;
-        var rsa_latest_confirmed = data.rsa_latest_confirmed;
-        var rsa_latest_deaths = data.rsa_latest_deaths;
+        var latest_update = data.rsa_latest_update;
+        var latest_tested = data.rsa_latest_tested;
+        var latest_confirmed = data.rsa_latest_confirmed;
+        var latest_deaths = data.rsa_latest_deaths;
 
-        $("#last_updated").text(rsa_latest_update);
-        $("#tests_conducted").text(rsa_latest_tested);
-        $("#confirmed_cases").text(rsa_latest_confirmed);
-        $("#deaths").text(rsa_latest_deaths);
+        $("#last_updated").text(latest_update);
+        $("#tests_conducted").text(latest_tested);
+        $("#confirmed_cases").text(latest_confirmed);
+        $("#deaths").text(latest_deaths);
     });
 };
 
 function updateSADashboard() {
     $.getJSON('widgets/latest_values.json', function( data ) {
-        var rsa_latest_update = data.rsa_latest_update;
-        var rsa_latest_tested = data.rsa_latest_tested;
-        var rsa_latest_confirmed = data.rsa_latest_confirmed;
-        var rsa_latest_deaths = data.rsa_latest_deaths;
+        var latest_update = data.rsa_latest_update;
+        var latest_tested = data.rsa_latest_tested;
+        var latest_confirmed = data.rsa_latest_confirmed;
+        var latest_deaths = data.rsa_latest_deaths;
 
-        $("#last_updated").text(rsa_latest_update);
-        $("#tests_conducted").text(rsa_latest_tested);
-        $("#confirmed_cases").text(rsa_latest_confirmed);
-        $("#deaths").text(rsa_latest_deaths);
+        $("#last_updated").text(latest_update);
+        $("#tests_conducted").text(latest_tested);
+        $("#confirmed_cases").text(latest_confirmed);
+        $("#deaths").text(latest_deaths);
     });
 };
