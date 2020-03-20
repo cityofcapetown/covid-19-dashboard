2ecce77aec62e1f0887a6c8d40de3a727c0ae509
Gordon Inggs <gordon.e.inggs@ieee.org>
2020-03-20 12:11:46 +0200

    Switching City to WC stats

--- assets/js/update_dash.js
+++ assets/js/update_dash.js
@@ -1,8 +1,8 @@
 function updateCityDashboard() {
     $.getJSON('widgets/latest_values.json', function( data ) {
-        var latest_update = data.rsa_latest_update;
+        var latest_update = data.wc_latest_update;
         var latest_tested = data.rsa_latest_tested;
-        var latest_confirmed = data.rsa_latest_confirmed;
+        var latest_confirmed = data.wc_latest_confirmed;
         var latest_deaths = data.rsa_latest_deaths;
 
         $("#last_updated").text(latest_update);
