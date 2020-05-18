823540a0935b0837dd2f5e27c793d27591b63465
Gordon Inggs <gordon.inggs@gmail.com>
2020-05-18 06:14:17 +0200

    Bugfix for navbar generation

--- assets/js/update_dash.js
+++ assets/js/update_dash.js
@@ -197,7 +197,7 @@ function addNavbar(targetDiv) {
                 'aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">' +
             '<span class="navbar-toggler-icon"></span>' +
         '</button>' +
-        '<div class="collapse navbar-collapse" id="navbarNav" class="h-100">' +
+        '<div class="collapse navbar-collapse" id="navbarNav">' +
             '<ul class="navbar-nav">' +
               navbarItemsHtml +
             '</ul>' +
